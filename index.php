2dfdb4beae62a2798a3891a2af4eabc06e1242df
Lakmal <lakmalniranga@gmail.com>
2016-09-30 14:09:39 +0530

    Basic HTML5 Template updated

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php 
-	echo "string";
- ?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>LMS</title>
+  <meta name="Learning Management System">
+  <meta name="author" content="NSBM 16.1 Group 01">
+
+  <link rel="stylesheet" href="assets/css/styles.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <script src="assets/js/scripts.js"></script>
+</body>
+</html>
