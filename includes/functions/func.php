e3d96b1e848a575576eec9cee66b374e279108c4
Chathula Sampath <schathula@gmail.com>
2016-10-03 02:15:42 +0530

    added hashing helper functions

--- includes/functions/func.php
+++ includes/functions/func.php
@@ -3,3 +3,15 @@
 function escape($string) {
   return htmlentities($string, ENT_QUOTES, 'UTF-8');
 }
+
+function generateHash($password) {
+    if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+        $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+        return crypt($password, $salt);
+    }
+}
+
+function verify($password, $hashedPassword) {
+    return crypt($password, $hashedPassword) == $hashedPassword;
+}
+
