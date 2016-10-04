d938e091aaf5805f458dfe14f9a52c44e4be1112
Chathula Sampath <schathula@gmail.com>
2016-10-04 10:47:16 +0530

    added root

--- core/init.php
+++ core/init.php
@@ -4,6 +4,9 @@ session_start();
 // debug mode | default : Off
 ini_set('display_errors', 'On');
 
+
+define('ROOT', "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+
 $GLOBALS['config'] = array(
     /**
     * database configurations for the MySQL
@@ -11,7 +14,7 @@ $GLOBALS['config'] = array(
     'mysql'     => array(
       'host'      => '127.0.0.1',
       'username'  => 'root',
-      'password'  => '1995cd',
+      'password'  => '',
       'db'        => 'lms'
     ),
 
@@ -26,6 +29,7 @@ $GLOBALS['config'] = array(
     'session'   => array(
       'session_name'  => ' user'
     )
+
 );
 
 //autoload required classes
