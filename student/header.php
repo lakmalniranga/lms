4225e692b041dcce48ad5621cd761dd3279c71b5
Chathula Sampath <schathula@gmail.com>
2016-10-25 09:32:53 +0530

    fixed header.php issue

--- student/header.php
+++ student/header.php
@@ -1,14 +1,11 @@
 <?php
-
 /**
 * added for the extra good security practice
 */
 if(count(get_included_files()) ==1) header("Location: /404.php");
 if(count(get_included_files()) ==1) exit("Page not found");
-
 include_once( dirname(dirname(__FILE__)) .'/classes/Config.php');
 require_once 'core/init.php';
-
 ?>
 
 <html lang="en">
@@ -17,13 +14,13 @@ require_once 'core/init.php';
 
   <title>LMS</title>
   <meta name="Learning Management System">
-  <meta name="author" content="NSBM 16.1 Group 01">
+  <meta name="author" content="NSBM 16.1 Group 01"> 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-
+  
   <link rel="stylesheet" href="<?php echo ROOT . "assets/css/style.css"; ?>">
 
   <!--[if lt IE 9]>
@@ -32,7 +29,7 @@ require_once 'core/init.php';
 </head>
 <body>
   <div class="container">
-
+  
       <header>
         <div class="row">
           <div class="column column-8">
@@ -40,11 +37,11 @@ require_once 'core/init.php';
           </div>
 
           <div class="column column-4">
-
+            
           </div>
         </div>
-
-        <div class="row">
+        
+        <div class="row">    
           <nav class="column column-12 navbar">
             <ul>
               <li><a href="index.php">Home</a></li>
@@ -53,12 +50,6 @@ require_once 'core/init.php';
               <li><a href="#">Chat</a></li>
 
               <?php if ($user->isLoggedIn()) : ?>
-<<<<<<< HEAD
-                <li class="right"><a href="logout.php">Logout</a></li>
-                <li class="right"><a href="profile.php">My Profile</a></li>
-              <?php else : ?>
-                <li class="right"><a href="login.php">Login</a></li>
-=======
                   <li class="right">
                     <a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
                   </li>
@@ -79,11 +70,11 @@ require_once 'core/init.php';
                         TEACHER
                       </a>
                     </li>
+                  <?php else: Redirect::to('index.php'); ?>
                   <?php endif; ?>
 
                 <?php else: ?>
-                  <li class="right"><a href="login.php">LOGIN</i></a></li>
->>>>>>> 8254b6e9cd6448e7909336ff7ea54a633f0379d5
+                  <?php Redirect::to('index.php');  ?>
               <?php endif; ?>
             </ul>
           </nav>
